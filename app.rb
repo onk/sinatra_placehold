b036db45099dbe43838739a5a09470077dc2247f
onk <takafumi.onaka@gmail.com>
2014-02-11 07:38:52 +0900

    label support

--- app.rb
+++ app.rb
@@ -1,17 +1,19 @@
 require "sinatra/base"
 require "RMagick"
+require "cgi"
 
 class App < Sinatra::Base
-  get "/:size/?:bgcolor?/?:color?" do
+  get %r{/(?<size>[0-9x]+)/?(?<bgcolor>[0-9a-fA-F]+)?/?(?<color>[0-9a-fA-F]+)?(&text=)?(?<text>.*)?} do
     content_type 'image/png'
     width, height = params[:size].split("x").map(&:to_i)
     height ||= width # if input width only
     bgcolor = params[:bgcolor] || "cccccc"
-    color = params[:color] || "000000"
+    color   = params[:color]   || "000000"
+    text    = params[:text] ? CGI.unescape(params[:text]) : "#{width}x#{height}"
     img = Magick::Image.new(width, height) {
       self.background_color = "##{bgcolor}"
     }
-    Magick::Draw.new.annotate(img, 0, 0, 0, 0, "#{width}x#{height}") do
+    Magick::Draw.new.annotate(img, 0, 0, 0, 0, text) do
       self.gravity = Magick::CenterGravity
       self.fill = "##{color}"
     end
