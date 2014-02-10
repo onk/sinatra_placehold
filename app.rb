bf5957aa07049a3e7869cb966c2429b318b9e24e
onk <takafumi.onaka@gmail.com>
2014-02-11 07:32:27 +0900

    color support

--- app.rb
+++ app.rb
@@ -2,14 +2,18 @@ require "sinatra/base"
 require "RMagick"
 
 class App < Sinatra::Base
-  get '/:size' do
+  get "/:size/?:bgcolor?/?:color?" do
     content_type 'image/png'
-
     width, height = params[:size].split("x").map(&:to_i)
     height ||= width # if input width only
-    img = Magick::Image.new(width, height) { self.background_color = "#cccccc" }
+    bgcolor = params[:bgcolor] || "cccccc"
+    color = params[:color] || "000000"
+    img = Magick::Image.new(width, height) {
+      self.background_color = "##{bgcolor}"
+    }
     Magick::Draw.new.annotate(img, 0, 0, 0, 0, "#{width}x#{height}") do
       self.gravity = Magick::CenterGravity
+      self.fill = "##{color}"
     end
 
     img.format = 'png'
